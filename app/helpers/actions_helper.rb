efc9c4c17795941dbb67cbab77afaffc08733fe3
Myles Skinner <mskinner@Dont-Steal-This-Name.local>
2019-03-04 12:51:30 -0500

    TCS-0128: Simplify action_helper

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,6 +1,5 @@
 module ActionsHelper
   def build_action
-
     controllers_with_actions = %w[users]
 #
 # all the potential controllers:
@@ -9,14 +8,12 @@ module ActionsHelper
     if (controllers_with_actions.include?(controller_name))
       case action_name
       when 'index'
-        action = { label: I18n.t('action.new', model: controller_name.classify),
-                   url: url_for(controller: controller_name, action: :new) }
+        return { label: I18n.t('action.new', model: controller_name.classify),
+                 url: url_for(controller: controller_name, action: :new) }
       when 'show'
-        action = { label: I18n.t('action.edit', model: controller_name.classify),
-                   url: url_for(controller: controller_name, action: :edit, id: params[:id]) }
+        return { label: I18n.t('action.edit', model: controller_name.classify),
+                 url: url_for(controller: controller_name, action: :edit, id: params[:id]) }
       end
     end
-
-    action
   end
 end
