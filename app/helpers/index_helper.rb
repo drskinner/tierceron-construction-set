105f2d0815abfdb393315dcb53a3581fb6e379ab
Myles Skinner <mskinner@Dont-Steal-This-Name.local>
2019-06-07 00:20:20 -0400

    TCS-0151: Fix indent

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -19,7 +19,7 @@ module IndexHelper
   end
 
   def pagination_info(page:, page_count:, full_count:)
-page ||= 1
+    page ||= 1
     my_start = (page.to_i - 1) * 20 + 1
     raw "Displaying #{my_start}&ndash;#{my_start + page_count - 1} of #{full_count} records."
   end
